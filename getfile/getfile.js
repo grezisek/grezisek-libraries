41711907401d2cb9e5dee6421752ca45d0a419d2
grezisek <38856057+grezisek@users.noreply.github.com>
2021-04-18 14:26:01 +0200

    Update getfile.js
    added internal translation to string

--- getfile/getfile.js
+++ getfile/getfile.js
@@ -1,23 +1,26 @@
 async function getFile(filePath = "") {
     //solve cache
-    const fileHeadRequest = new Request(filePath, {method: "HEAD"});
+    const fileHeadRequest = new Request(filePath, { method: "HEAD" });
     const fileHeadResponse = await fetch(fileHeadRequest);
     const fileLastModified = fileHeadResponse.headers.get("Last-Modified");
 
-    let local = localStorage.getItem("grezisek-"+filePath);
+    let local = localStorage.getItem("grezisek-" + filePath);
     if (local) local = JSON.parse(local)
-    else local = {lastModified: "Thu, 01 Jan 1970 00:00:00 GMT"};
+    else local = { lastModified: "Thu, 01 Jan 1970 00:00:00 GMT" };
 
     //cached return
-    if (local.lastModified == fileLastModified) return local.data;
-    
+    if (local.lastModified == fileLastModified) return JSON.parse(local.data);
+
     //new fetch
-    const fileRequest = new Request(filePath, {cache: "reload"});
+    const fileRequest = new Request(filePath, { cache: "reload" });
     const fileResponse = await fetch(fileRequest);
     const fileData = await fileResponse.text();
 
-    localStorage.setItem("grezisek-"+filePath, JSON.stringify({lastModified: fileLastModified, data: fileData}));
-    if (JSON.parse(localStorage.getItem("grezisek-"+filePath).data) != fileData) localStorage.removeItem("grezisek-"+filePath);
+    localStorage.setItem(
+        "grezisek-" + filePath,
+        JSON.stringify({ "lastModified": fileLastModified, "data": JSON.stringify(fileData) })
+    );
+    if (JSON.parse(JSON.parse(localStorage.getItem("grezisek-" + filePath)).data) != fileData) localStorage.removeItem("grezisek-" + filePath);
 
     //live return
     return fileData;
